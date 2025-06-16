221522e2f99bf74663ccc92c0cdf712f3c2e8bce
Sahil <sahilchopra1975@gmail.com>
2025-06-16 23:41:37 +0530

    Navbar Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import Layout from './Layout'
 import Home from './pages/Home'
+import Contact from './pages/Contact'
 
 const App = () => {
 
@@ -8,6 +9,7 @@ const App = () => {
     createRoutesFromElements(
       <Route path='/' element={<Layout/>} >
         <Route path='' element={<Home />} />
+        <Route path='contact' element={<Contact />} />
 
       </Route>
     )
