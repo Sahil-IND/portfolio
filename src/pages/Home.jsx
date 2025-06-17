ffd165eb122d18df82e98283fa437f7cba125f08
Sahil <sahilchopra1975@gmail.com>
2025-06-17 14:46:26 +0530

    About Me Done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import MyComponent from '../components/MyComponent'
 import devImg from '../assets/dev_4.png'
+import devImage from '../assets/dev.png'
 
 const Home = () => {
   return (
@@ -18,6 +19,25 @@ const Home = () => {
         </div>
 
         <hr className='border-00; bg-blue-600 h-[1.8px] my-[40px] mx-[84px]'/>
+
+        <div className="about w-[100%] h-[40rem] ">
+          <h2 className='text-left ml-24 text-5xl mt-20 font-semibold'>About Me</h2>
+          <div className="w-[100%] h-[40rem] flex justify-around">
+            <div className='w-[55%] h-[80%] flex justify-center items-center text-2xl ml-24'>
+            <p className='text-gray-400'>I am a Frontend developer with a year of experience in building web applications. I specialize in the React and other modern technologies to build real-world projects with seamless user experiences and master modern web technologies. Outside of coding, I enjoy continuous learning and sharing knowledge with others. </p>
+
+            
+          </div>
+          <div className='flex justify-center w-[35%] h-[80%] mr-6'>
+              <img src={devImage} alt="profile image" loading='lazy' className="w-full h-auto object-contain mt-4"  />
+            </div>
+          </div>
+          
+        </div>
+
+        <hr className='border-00; bg-blue-600 h-[1.8px] my-[40px] mx-[84px]'/>
+
+        
     </main>
   )
 }
